af643d56f4306718c8c21c953be505e7735922ec
gabriell690 <gabriellbatista096@gmail.com>
2026-06-25 22:04:19 -0300

    ajustar lancamentos direcionamento

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -2,7 +2,7 @@ import { MessageCircle } from "lucide-react";
 
 export default function WhatsAppButton() {
   return (
-    <div className="fixed bottom-6 right-6 z-[999] flex flex-col items-center">
+    <div className="fixed bottom-6 right-6 z-999 flex flex-col items-center">
 
       {/* Foto Edgar */}
      <img
