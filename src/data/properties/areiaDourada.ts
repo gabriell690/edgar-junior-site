fdfa8a0aa9a85a8e13bebf3ee3b406207381047b
gabriell690 <gabriellbatista096@gmail.com>
2026-07-22 22:03:13 -0300

    atualizando empreendimentos

--- src/data/properties/areiaDourada.ts
+++ src/data/properties/areiaDourada.ts
@@ -1,1 +1,23 @@
-export const areiaDourada = [];
+import type { Property } from "../../types/Property";
+
+export const areiaDourada: Property[] = [
+
+    {
+    slug: "Marine",
+    name: "Marine",
+    neighborhood: "Areia Dourada",
+    location: "Areia Dourada - João Pessoa/PB",
+    price: " R$601.000,00",
+    delivery: "Agosto de 2029",
+    category: "1,2 e 3 Quartos",
+    description: "",
+    cover: "/empreendimentos/areia-dourada/marine/capa.webp",
+    images: ["/empreendimentos/areia-dourada/marine/1.webp",
+             "/empreendimentos/areia-dourada/marine/2.webp",
+             "/empreendimentos/areia-dourada/marine/3.webp",
+             "/empreendimentos/areia-dourada/marine/4.webp",
+             "/empreendimentos/areia-dourada/marine/5.webp",
+             "/empreendimentos/areia-dourada/marine/6.webp",],
+  },
+
+];
--- src/data/properties/bancarios.ts
+++ src/data/properties/bancarios.ts
@@ -1,1 +1,23 @@
-export const bancarios = [];
+import type { Property } from "../../types/Property";
+
+export const bancarios: Property[] = [
+
+    {
+    slug: "EQ Raizes",
+    name: "EQ Raizes",
+    neighborhood: "Bancários",
+    location: "Bancários - João Pessoa/PB",
+    price: " R$306.000,00",
+    delivery: "Junho de 2026",
+    category: "2 Quartos",
+    description: "",
+    cover: "/empreendimentos/bancarios/eq-marines/capa.webp",
+    images: ["/empreendimentos/bancarios/eq-marines/1.webp",
+             "/empreendimentos/bancarios/eq-marines/2.webp",
+             "/empreendimentos/bancarios/eq-marines/3.webp",
+             "/empreendimentos/bancarios/eq-marines/4.webp",
+             "/empreendimentos/bancarios/eq-marines/5.webp",
+             "/empreendimentos/bancarios/eq-marines/6.webp",],
+  },
+
+];
