e843e581eff34d97e3034b1a884f6d5c9af94788
gabriell690 <gabriellbatista096@gmail.com>
2026-07-27 01:21:45 -0300

    novos empreendimentos

--- src/data/properties/jardimLuna.ts
+++ src/data/properties/jardimLuna.ts
@@ -1,1 +1,23 @@
-export const jardimLuna = [];
+import type { Property } from "../../types/Property";
+
+export const jardimLuna: Property[] = [
+
+ {
+    slug: "Live Residence",
+    name: "Live Residence",
+    neighborhood: "Jardim Luna",
+    location: "Jardim Luna - João Pessoa/PB",
+    price: " R$221.998,00",
+    delivery: "Novembro de 2029",
+    category: "Flats, 1 e 2 Quartos",
+    description: "",
+    cover: "/empreendimentos/formosa/veleiro/capa.webp",
+    images: ["/empreendimentos/formosa/veleiro/1.webp",
+      "/empreendimentos/formosa/veleiro/2.webp",
+      "/empreendimentos/formosa/veleiro/3.webp",
+      "/empreendimentos/formosa/veleiro/4.webp",
+      "/empreendimentos/formosa/veleiro/5.webp",
+      "/empreendimentos/formosa/veleiro/6.webp",
+    ],
+  },
+  ];
